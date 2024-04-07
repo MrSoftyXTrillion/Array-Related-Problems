c5954519bca65d3e9eae87913267bb9b69b52eea
Mr.Softy <123297701+MrSoftyXTrillion@users.noreply.github.com>
2024-04-07 14:40:55 +0600

    Update 01.Print_Array.c

--- 01.Print_Array.c
+++ 01.Print_Array.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 int main ()
 {
-    int i,n;
+    int n,i;
     scanf ("%d",&n);
-    int a[n];
-    for (i=1;i<=n;i++)
+    int arr[n];
+    for (i=0;i<n;i++)
     {
-        scanf ("%d",&a[i]);
+        scanf ("%d",&arr[i]);
     }
-    for (i=1;i<=n;i++)
+    for (i=0;i<n;i++)
     {
-        printf ("%d\n",a[i]);
+        printf ("%d ",arr[i]);
     }
     return 0;
 }
