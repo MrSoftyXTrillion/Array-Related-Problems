afc3e418696661a9b0963cd1f88a1fecc1180251
Mr.Softy <123297701+MrSoftyXTrillion@users.noreply.github.com>
2024-04-07 15:00:05 +0600

    Update 05.Print_last_half_of_an_array.c

--- 05.Print_last_half_of_an_array.c
+++ 05.Print_last_half_of_an_array.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 int main ()
 {
-    int i,start,end,n;
+    int i,n;
     scanf ("%d",&n);
-    int a[n];
-    for (i=1;i<=n;i++)
+    int arr[n];
+    for  (i=0;i<n;i++)
     {
-        scanf ("%d",&a[i]);
+        scanf ("%d",&arr[i]);
     }
-    for (i=(n/2)+1;i<=n;i++)
+    for (i=n/2+1;i<n;i++)
     {
-        printf ("%d\n",a[i]);
+        printf ("%d ",arr[i]);
     }
     return 0;
 }
