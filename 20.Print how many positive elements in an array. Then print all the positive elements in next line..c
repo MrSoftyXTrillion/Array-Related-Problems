1ca1e1bbe057939189865c3b134986c3e3e2b301
Mr.Softy <123297701+MrSoftyXTrillion@users.noreply.github.com>
2024-04-07 20:14:34 +0600

    Create 20.Print how many positive elements in an array. Then print all the positive elements in next line..c

---
+++ 20.Print how many positive elements in an array. Then print all the positive elements in next line..c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main ()
+{
+    int i,n,count=0;
+    scanf ("%d",&n);
+    int arr[n];
+    for (i=0;i<n;i++)
+    {
+        scanf ("%d",&arr[i]);
+    }
+    for (i=0;i<n;i++)
+    {
+        if (arr[i]>0)
+        {
+            count++;
+        }
+        printf ("%d ",arr[i]);
+    }
+    printf ("\n");
+    printf ("%d\n",count);
+    return 0;
+}
