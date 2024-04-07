cd95732da4f480a64d148c9b681b951c1e5bb35b
Mr.Softy <123297701+MrSoftyXTrillion@users.noreply.github.com>
2024-04-07 14:52:50 +0600

    Update 03.Print_all_the_odd_elements_of_an_array.c

--- 03.Print_all_the_odd_elements_of_an_array.c
+++ 03.Print_all_the_odd_elements_of_an_array.c
@@ -3,16 +3,16 @@ int main ()
 {
     int i,n;
     scanf ("%d",&n);
-    int a[n];
-    for (i=1;i<=n;i++)
+    int arr[n];
+    for (i=0;i<n;i++)
     {
-        scanf ("%d",&a[i]);
+        scanf ("%d",&arr[i]);
     }
-    for (i=1;i<=n;i++)
+    for (i=0;i<n;i++)
     {
-        if (a[i]%2!=0)
+        if (arr[i]%2!=0)
         {
-            printf ("%d\n",a[i]);
+            printf ("%d ",arr[i]);
         }
     }
     return 0;
