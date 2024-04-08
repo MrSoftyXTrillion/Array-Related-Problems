2ec3496388b39ac0fa218e7cd77d0a55c08bf066
Mr.Softy <123297701+MrSoftyXTrillion@users.noreply.github.com>
2024-04-09 02:33:05 +0600

    Create 64.Print 2D array in row major order.c

---
+++ 64.Print 2D array in row major order.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main ()
+{
+    int i,j,n,m;
+    scanf ("%d %d",&n,&m);
+    int arr[i][j];
+    for (i=0;i<n;i++)
+    {
+        for (j=0;j<m;j++)
+        {
+            scanf ("%d",&arr[i][j]);
+        }
+    }
+    for (i=0;i<n;i++)
+    {
+        for (j=0;j<m;j++)
+        {
+            printf ("%d ",arr[i][j]);
+        }
+    }
+    return 0;
+}
