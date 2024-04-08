79e33bd13158b6aabd0801bcbf3f9eae5e47c2f2
Mr.Softy <123297701+MrSoftyXTrillion@users.noreply.github.com>
2024-04-09 03:56:34 +0600

    Create 69.Print 2D array in column major order.c

---
+++ 69.Print 2D array in column major order.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main ()
+{
+    int i,j,n,m;
+    scanf ("%d %d",&n,&m);
+    int arr[n][m];
+    for (i=0;i<n;i++)
+    {
+        for (j=0;j<m;j++)
+        {
+            scanf ("%d",&arr[i][j]);
+        }
+    }
+    for (j=0;j<m;j++)
+    {
+        for (i=0;i<n;i++)
+        {
+            printf ("%d ",arr[i][j]);
+        }
+    }
+    return 0;
+}
